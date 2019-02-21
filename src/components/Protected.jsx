df3aee26387f2c2028400175eaaeb88c16e4d744
Azat Akhunov <azat.akhunov@protonmail.com>
2019-02-21 06:41:32 +0300

    [CF] main page update

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -6,9 +6,8 @@ class Protected extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
-            status: 'bot stopped', error: false, running: false
-    };
+            status: 'bot stopped', create: '', stop: ''
+        };
         this.updateMessage = this.updateMessage.bind(this);
     }
 
@@ -28,7 +27,39 @@ class Protected extends React.Component {
         }).then(result => {
             const parsedResponse = JSON.parse(JSON.stringify(result.data));
             this.setState({
-                status: parsedResponse.status, error: parsedResponse.hasError, running: parsedResponse.running
+                status: parsedResponse.status
+            })
+        }).catch(result => {
+            console.log(result);
+        })
+    }
+
+    botStart(){
+        axios({
+            url: 'https://botmasterzzz.com/admin/bot/start',
+            method: 'post',
+            data: {
+                access_token: localStorage.getItem('token')
+            }
+        }).then(result => {
+            this.setState({
+                create: result.data
+            })
+        }).catch(result => {
+            console.log(result);
+        })
+    }
+
+    botStop(){
+        axios({
+            url: 'https://botmasterzzz.com/admin/bot/stop',
+            method: 'post',
+            data: {
+                access_token: localStorage.getItem('token')
+            }
+        }).then(result => {
+            this.setState({
+                stop: result.data
             })
         }).catch(result => {
             console.log(result);
@@ -42,6 +73,8 @@ class Protected extends React.Component {
                 <h1>BOT error: {this.state.error}</h1>
                 <h1>BOT running: {this.state.running}</h1>
                 <button onClick={this.componentDidMount}>Status</button>
+                <button onClick={this.botStart}>Start</button>
+                <button onClick={this.botStop}>Stop</button>
             </div>
         );
     }
