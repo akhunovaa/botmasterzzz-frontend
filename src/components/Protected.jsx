800a31e23ad2a2a03c48f3432bf24eba334f84a0
Azat Akhunov <azat.akhunov@protonmail.com>
2019-02-21 06:30:18 +0300

    [CF] main page update

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -14,7 +14,7 @@ class Protected extends React.Component {
 
     updateMessage() {
         this.setState({
-            message: 'my friend (from changed state)!'
+            message: 'Добро пожаловать!'
     });
     }
 
