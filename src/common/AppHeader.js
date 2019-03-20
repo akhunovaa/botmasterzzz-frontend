621ebd2b95ed9c0e27045bfaacd3d12d2196427e
Azat Akhunov <azat.akhunov@protonmail.com>
2019-03-20 18:38:14 +0300

    [CF] update главной страницы

--- src/common/AppHeader.js
+++ src/common/AppHeader.js
@@ -1,39 +1,38 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './AppHeader.css';
+import '../styles/style.css';
 
 class AppHeader extends Component {
     render() {
         return (
-            <header className="app-header">
-                <div className="container">
-                    <div className="app-branding">
-                        <Link to="/" className="app-title">Botmasterzzz</Link>
+            <header className="header">
+                    <div className="active">
+                        <Link to="/">Botmasterzzz</Link>
                     </div>
-                    <div className="app-options">
-                        <nav className="app-nav">
+                    <div className="b_nav_box">
+                        <nav className="b_nav_links">
                             { this.props.authenticated ? (
                                 <ul>
                                     <li>
-                                        <NavLink to="/profile">Профиль</NavLink>
+                                        <NavLink className="active" to="/profile">Профиль</NavLink>
                                     </li>
                                     <li>
-                                        <a onClick={this.props.onLogout}>Выйти</a>
+                                        <a className="active" onClick={this.props.onLogout}>Выйти</a>
                                     </li>
                                 </ul>
                             ): (
                                 <ul>
                                     <li>
-                                        <NavLink to="/login">Авторизация</NavLink>
+                                        <NavLink className="active" to="/login">Авторизация</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to="/signup">Регистрация</NavLink>
+                                        <NavLink className="active" to="/signup">Регистрация</NavLink>
                                     </li>
                                 </ul>
                             )}
                         </nav>
                     </div>
-                </div>
             </header>
         )
     }
