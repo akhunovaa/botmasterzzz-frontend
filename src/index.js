4f9daf5125f7f831fa377b4ad35824178a31c713
Azat Akhunov <azat.akhunov@protonmail.com>
2019-03-22 00:26:25 +0300

    [CF] update главной страницы

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
