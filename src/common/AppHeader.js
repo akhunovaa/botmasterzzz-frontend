23740788c3f94f031eff6e803a0913ee4ab7e675
Azat Akhunov <azat.akhunov@protonmail.com>
2019-03-21 19:20:45 +0300

    [CF] update главной страницы

--- src/common/AppHeader.js
+++ src/common/AppHeader.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './AppHeader.css';
 import '../styles/style.css';
+import mainLogo from '../img/logo.png';
 
 class AppHeader extends Component {
     render() {
         return (
             <header className="header">
+                <div className="b_nav_contacts pull-xs-right"/>
+                <div className="b_nav_contacts pull-xs-right">
+                    <div className="b_nav_contacts_phone"><a
+                        href="mailto:admin@botmasterzzz.com">admin@botmasterzzz.com</a></div>
+                    <div className="b_nav_contacts_phone"><a href="https://t.me/balsakas">@balsakas</a></div>
+                </div>
                     <div className="active">
                         <Link to="/">Botmasterzzz</Link>
                     </div>
+                <a href="/" className="b_nav_link_home pull-xs-left">
+                    <div className="b_nav_logo pull-xs-left">
+                        <img src={mainLogo} alt="Botmasterzzz Logo"/>
+                    </div>
+                    <div className="b_nav_brand pull-xs-left">
+                        Botmasterzzz
+                    </div>
+                </a>
                     <div className="b_nav_box">
                         <nav className="b_nav_links">
                             { this.props.authenticated ? (
@@ -34,6 +49,7 @@ class AppHeader extends Component {
                         </nav>
                     </div>
             </header>
+
         )
     }
 }
--- src/home/Home.js
+++ src/home/Home.js
@@ -6,26 +6,26 @@ class Home extends Component {
     render() {
         return (
             <section className="header">
-                <nav className="b_nav clearfix">
-                    <div className="b_nav_contacts pull-xs-right">
+                {/*<nav className="b_nav clearfix">*/}
+                    {/*<div className="b_nav_contacts pull-xs-right">*/}
 
-                    </div>
-                    <div className="b_nav_contacts pull-xs-right">
-                        <div className="b_nav_contacts_phone"><a
-                            href="mailto:admin@botmasterzzz.com">admin@botmasterzzz.com</a></div>
-                        <div className="b_nav_contacts_phone"><a href="https://t.me/balsakas">@balsakas</a></div>
-                    </div>
+                    {/*</div>*/}
+                    {/*<div className="b_nav_contacts pull-xs-right">*/}
+                        {/*<div className="b_nav_contacts_phone"><a*/}
+                            {/*href="mailto:admin@botmasterzzz.com">admin@botmasterzzz.com</a></div>*/}
+                        {/*<div className="b_nav_contacts_phone"><a href="https://t.me/balsakas">@balsakas</a></div>*/}
+                    {/*</div>*/}
 
-                    <a href="/" className="b_nav_link_home pull-xs-left">
-                        <div className="b_nav_logo pull-xs-left">
-                            <img src={mainLogo} alt="Botmasterzzz Logo"/>
-                        </div>
-                        <div className="b_nav_brand pull-xs-left">
-                            Botmasterzzz
-                        </div>
-                    </a>
+                    {/*<a href="/" className="b_nav_link_home pull-xs-left">*/}
+                        {/*<div className="b_nav_logo pull-xs-left">*/}
+                            {/*<img src={mainLogo} alt="Botmasterzzz Logo"/>*/}
+                        {/*</div>*/}
+                        {/*<div className="b_nav_brand pull-xs-left">*/}
+                            {/*Botmasterzzz*/}
+                        {/*</div>*/}
+                    {/*</a>*/}
 
-                </nav>
+                {/*</nav>*/}
             </section>
         )
     }
