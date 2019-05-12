408c1f0b168955b79d30af3469c98ca10d32ab99
Azat Akhunov <azat.akhunov@protonmail.com>
2019-05-12 03:00:37 +0300

    [NF] project update page inited

--- src/project/Project.js
+++ src/project/Project.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import './Project.css';
-import {Button, Header, Icon, Segment} from "semantic-ui-react";
+import TestLogo from '../img/github-logo.png';
+import TestLogo2 from '../img/google-logo.png';
+import TestLogo3 from '../img/fb-logo.png';
+import {Button, Header, Icon, Segment, Grid, Placeholder, Image, Item} from "semantic-ui-react";
 
 
 class Project extends Component {
@@ -44,8 +47,82 @@ class Project extends Component {
                         </Segment>
                 </div>
 
-                <h2>Создание: {this.state.createdButtonText.toString()}</h2>
-                <h2>Удаленные: {this.state.deletedButtonText.toString()}</h2>
+                <div className='project-layer'>
+                    <Grid columns={3} stackable>
+                        <Grid.Column>
+                            <Segment>
+                                <div className='project-cell-header'>
+                                    <Header floated='left'>
+                                        <Image size='mini' src={TestLogo} circular/>
+                                        <Header.Content>
+                                            Noname project
+                                            <Header.Subheader>Noname_bot</Header.Subheader>
+                                        </Header.Content>
+                                    </Header>
+                                </div>
+                                <div className='project-cell-ico-body'>
+                                    <Icon size='big' color='blue' name='telegram'/>
+                                    <Icon size='big' color='green' name='whatsapp'/>
+                                    <Icon size='big' color='blue' name='vk'/>
+                                </div>
+                                <div className='project-cell-update-body'>
+                                    <Button size='big' basic onClick={this.showCreate()} icon="pencil"
+                                            content='Настроить'/>
+                                    <Button size='big' basic onClick={this.showCreate()} icon="group"
+                                            content='15к'/>
+                                </div>
+                            </Segment>
+
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Segment>
+                                <div className='project-cell-header'>
+                                    <Header floated='left'>
+                                        <Image size='small' src={TestLogo2} circular/>
+                                        <Header.Content>
+                                            Second project
+                                            <Header.Subheader>Second_bot</Header.Subheader>
+                                        </Header.Content>
+                                    </Header>
+                                </div>
+                                <div className='project-cell-ico-body'>
+                                    <Icon size='big' color='blue' name='telegram'/>
+                                    <Icon size='big' color='green' name='whatsapp'/>
+                                </div>
+                                <div className='project-cell-update-body'>
+                                    <Button size='big' basic onClick={this.showCreate()} icon="pencil"
+                                            content='Настроить'/>
+                                    <Button size='big' basic onClick={this.showCreate()} icon="group"
+                                             content='134'/>
+                                </div>
+                            </Segment>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Segment>
+                                <div className='project-cell-header'>
+                                    <Header floated='left'>
+                                        <Image size='small' src={TestLogo3} circular/>
+                                        <Header.Content>
+                                            Test project
+                                            <Header.Subheader>Test_bot</Header.Subheader>
+                                        </Header.Content>
+                                    </Header>
+                                </div>
+                                <div className='project-cell-ico-body'>
+                                    <Icon size='big' color='blue' name='telegram'/>
+                                </div>
+                                <div className='project-cell-update-body'>
+                                    <Button size='big' basic onClick={this.showCreate()} icon="pencil"
+                                            content='Настроить'/>
+                                    <Button size='big' basic onClick={this.showCreate()} icon="group"
+                                            content='567'/>
+                                </div>
+                            </Segment>
+                        </Grid.Column>
+                    </Grid>
+                </div>
             </div>
         );
     }
