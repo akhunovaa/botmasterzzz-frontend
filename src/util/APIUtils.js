394bed5eac28461279b447502b973ca57c2c23be
Azat Akhunov <azat.akhunov@protonmail.com>
2019-05-25 20:07:24 +0300

    [BF] content type header deleted -> for data uploader

--- src/util/APIUtils.js
+++ src/util/APIUtils.js
@@ -23,6 +23,29 @@ const request = (options) => {
         );
 };
 
+const requestImage = (options) => {
+    const headers = new Headers({
+        'Content-Type': '',
+    });
+
+    if(localStorage.getItem(ACCESS_TOKEN)) {
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
+    }
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+        .then(response =>
+            response.json().then(json => {
+                if(!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        );
+};
+
 export function getCurrentUser() {
     if(!localStorage.getItem(ACCESS_TOKEN)) {
         return Promise.reject("No access token set.");
@@ -59,7 +82,7 @@ export function profilePasswordUpdate(passDataRequest) {
 }
 
 export function profileImageUpdate(formData) {
-    return request({
+    return requestImage({
         url: API_BASE_URL + "/admin/user/image/upload",
         method: 'POST',
         body: formData
