ee205feb1aaf8778c4e3ca9db730f74cc8c9e378
Azat Akhunov <azat.akhunov@protonmail.com>
2019-05-27 11:16:45 +0300

    [BF] content type changed

--- src/util/APIUtils.js
+++ src/util/APIUtils.js
@@ -26,7 +26,7 @@ const request = (options) => {
 const requestImage = (options) => {
 
     const headers = new Headers({
-        'Content-Type': ''
+        'Content-Type': 'image/jpeg'
     });
 
     if(localStorage.getItem(ACCESS_TOKEN)) {
