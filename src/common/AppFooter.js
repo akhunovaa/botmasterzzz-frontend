50c5bf03a93a461f732f8fa875ba4eb419e0b08c
Azat Akhunov <azat.akhunov@protonmail.com>
2019-06-30 17:44:47 +0300

    [NF] footer component to authentificated users updated

--- src/common/AppFooter.js
+++ src/common/AppFooter.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './AppFooter.css';
 import footerBtn1 from '../img/global/footer_btn1.png';
 import footerBtn2 from '../img/global/footer_btn2.png';
@@ -13,50 +13,55 @@ class AppFooter extends Component {
     render() {
         return (
             <div id="footer">
-                <div id="footer-center">
-                    <div id="footer-block">
-                        <div>
-                            <b>Botmasterzzz</b>
-                            <a href="/feedback"><img className="footer_col_btn1" src={footerBtn3}/></a>
-                            <a href="#"><img className="footer_col_btn2" src={footerBtn1}/></a>
-                            <a href="#"><img className="footer_col_btn3" src={footerBtn2}/></a>
+                {this.props.authenticated ? (
+                        <div id="footer-center">
+                            <ul>
+                                <li><a href="#">О ПРОЕКТЕ</a></li>
+                                <li><a href="#">О НАС</a></li>
+                                <li><a href="/feedback">ОБРАТНАЯ СВЯЗЬ</a></li>
+                                <li><a href="#">КОНТАКТЫ</a></li>
+                                <a style={{float: 'right'}} onClick={this.props.onLogout}>ВЫЙТИ</a>
+
+                            </ul>
                         </div>
-                        <div>
-                            <img src={footer1}/>
-   	 	   	  <span>
+                ) : (
+                        <div id="footer-center">
+                            <div id="footer-block">
+                                <div>
+                                    <b>Botmasterzzz</b>
+                                    <a href="/feedback"><img className="footer_col_btn1" src={footerBtn3}/></a>
+                                    <a href="#"><img className="footer_col_btn2" src={footerBtn1}/></a>
+                                    <a href="#"><img className="footer_col_btn3" src={footerBtn2}/></a>
+                                </div>
+                                <div>
+                                    <img src={footer1}/>
+                                    <span>
    	 	   	  	 Все операции и диалоги с ботами синхронизируются на всех устройствах.
    	 	   	  </span>
-                        </div>
-                        <div>
-                            <img src={footer2}/>
-   	 	   	  <span>
+                                </div>
+                                <div>
+                                    <img src={footer2}/>
+                                    <span>
    	 	   	  	 Вы будете иметь полную информацию о текущем использовании Вашего бота.
    	 	   	  </span>
-                        </div>
-                        <div>
-                            <img src={footer3}/>
-   	 	   	  <span>
+                                </div>
+                                <div>
+                                    <img src={footer3}/>
+                                    <span>
    	 	   	  	 Управление всеми ботами на одной площадке
    	 	   	  </span>
-                        </div>
-                    </div>
-                    <ul>
-                        <li><a href="#">О ПРОЕКТЕ</a></li>
-                        <li><a href="#">О НАС</a></li>
-                        <li><a href="/feedback">ОБРАТНАЯ СВЯЗЬ</a></li>
-                        <li><a href="#">КОНТАКТЫ</a></li>
-
-                        {this.props.authenticated ? (
-                            <div>
-                                <a style={{float: 'right'}} onClick={this.props.onLogout}>ВЫЙТИ</a>
+                                </div>
                             </div>
-                        ) : (
-                            <div>
+                            <ul>
+                                <li><a href="#">О ПРОЕКТЕ</a></li>
+                                <li><a href="#">О НАС</a></li>
+                                <li><a href="/feedback">ОБРАТНАЯ СВЯЗЬ</a></li>
+                                <li><a href="#">КОНТАКТЫ</a></li>
                                 <NavLink style={{float: 'right'}} to="/login">ВХОД</NavLink>
-                            </div>
-                        )}
-                    </ul>
-                </div>
+                            </ul>
+                        </div>
+                )}
+
             </div>
         )
     }
