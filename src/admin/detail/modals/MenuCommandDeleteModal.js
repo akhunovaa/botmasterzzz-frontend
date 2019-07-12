1e061f654203a7fce94fb53f3b7e28b9149c8816
Azat Akhunov <azat.akhunov@protonmail.com>
2019-07-12 18:56:32 +0300

    [NF] bot token update function injected

--- src/admin/detail/modals/MenuCommandDeleteModal.js
+++ src/admin/detail/modals/MenuCommandDeleteModal.js
@@ -67,7 +67,7 @@ class MenuCommandDeleteModal extends Component {
                     Alert.warning(response.message);
                 } else {
                     this.props.onClose();
-                    this.forceUpdate();
+                    this.props.refresh();
                     Alert.success('Команда "' + response.command.command + '" успешно удалена');
                 }
             }).catch(error => {
