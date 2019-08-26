8efda4758f744f75df7665fdbd7a332a7a409b28
Azat Akhunov <azat.akhunov@protonmail.com>
2019-08-26 20:05:33 +0300

    [СF] context menu was updated for the registered users

--- src/common/MobileAppHeader.js
+++ src/common/MobileAppHeader.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './MobileAppHeader.css';
 import {Button, Portal, Segment, Header, Label, Divider, List} from "semantic-ui-react";
 
-
 class MobileAppHeader extends Component {
 
     constructor(props) {
@@ -33,59 +32,68 @@ class MobileAppHeader extends Component {
                     <path id="mobile_header_body" d="M 0 0 L 375 0 L 375 95 L 0 95 L 0 0 Z">
                     </path>
                 </svg>
-                <Portal
-                    closeOnTriggerClick
-                    openOnTriggerClick
-                    trigger={
-                        <Button className={"context_menu"} basic icon={'bars'} size={"massive"} />
-                    }
-                    onOpen={this.handleOpen}
-                    onClose={this.handleClose}>
-                    <Segment
-                        style={{position: 'fixed', top: '80px', zIndex: 1000}}>
-                        <List size={"big"}>
-                            <List.Item>
-                                <List.Icon name='mail' />
-                                <List.Content>
-                                    <a href="/feedback" style={{color: 'black'}}>Обратная связь</a>
-                                </List.Content>
-                            </List.Item>
-                            <List.Item>
-                                <Divider />
-                            </List.Item>
-                            <List.Item>
-                                <List.Content>
-                                    <a href="/" style={{color: 'black'}}>Новости</a>
-                                </List.Content>
-                            </List.Item>
-                            <List.Item>
-                                <List.Content>
-                                    <a href="/" style={{color: 'black'}}>О платформе</a>
-                                </List.Content>
-                            </List.Item>
-                            <List.Item>
-                                <List.Content>
-                                    <a href="/signup" style={{color: 'black'}}>Регистрация</a>
-                                </List.Content>
-                            </List.Item>
-                            <List.Item>
-                                <Divider />
-                            </List.Item>
-                            <List.Item>
-                                <List.Content>
-                                    <a href="/" style={{color: 'black', fontWeight: 'bold'}}>RU</a>
-                                </List.Content>
-                            </List.Item>
-                            <List.Item>
-                                <List.Content>
-                                    <a href="/" style={{color: 'black', fontWeight: 'bold'}}>EN</a>
-                                </List.Content>
-                            </List.Item>
-                        </List>
-                    </Segment>
-                </Portal>
+
                 {
                     this.props.authenticated ? (
+                        <div>
+                            <Portal
+                                closeOnTriggerClick
+                                openOnTriggerClick
+                                trigger={
+                                    <Button className={"context_menu"} basic icon={'bars'} size={"massive"} />
+                                }
+                                onOpen={this.handleOpen}
+                                onClose={this.handleClose}>
+                                <Segment
+                                    style={{position: 'fixed', top: '80px', zIndex: 1000}}>
+                                    <List size={"big"}>
+                                        <List.Item>
+                                            <List.Icon name='mail' />
+                                            <List.Content>
+                                                <a href="/feedback" style={{color: 'black'}}>Обратная связь</a>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <Divider />
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <a href="/" style={{color: 'black'}}>Новости</a>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <a href="/" style={{color: 'black'}}>О платформе</a>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <a href="/profile" style={{color: 'black'}}>Профиль</a>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <a href="/project" style={{color: 'black'}}>Мои боты</a>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <Divider />
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <a href="/" style={{color: 'black', fontWeight: 'bold'}}>RU</a>
+                                            </List.Content>
+                                        </List.Item>
+                                        <List.Item>
+                                            <List.Content>
+                                                <a href="/" style={{color: 'black', fontWeight: 'bold'}}>EN</a>
+                                            </List.Content>
+                                        </List.Item>
+                                    </List>
+                                </Segment>
+                            </Portal>
+
+
                 <div id="login">
                     <svg className="Rectangle_3">
                         <rect id="Rectangle_3" rx="16" ry="16" x="0" y="0" width="75" height="32">
@@ -97,8 +105,60 @@ class MobileAppHeader extends Component {
                         </a>
                     </div>
                 </div>
-
+                        </div>
                 ) : (
+                    <div>
+                        <Portal
+                            closeOnTriggerClick
+                            openOnTriggerClick
+                            trigger={
+                                <Button className={"context_menu"} basic icon={'bars'} size={"massive"} />
+                            }
+                            onOpen={this.handleOpen}
+                            onClose={this.handleClose}>
+                            <Segment
+                                style={{position: 'fixed', top: '80px', zIndex: 1000}}>
+                                <List size={"big"}>
+                                    <List.Item>
+                                        <List.Icon name='mail' />
+                                        <List.Content>
+                                            <a href="/feedback" style={{color: 'black'}}>Обратная связь</a>
+                                        </List.Content>
+                                    </List.Item>
+                                    <List.Item>
+                                        <Divider />
+                                    </List.Item>
+                                    <List.Item>
+                                        <List.Content>
+                                            <a href="/" style={{color: 'black'}}>Новости</a>
+                                        </List.Content>
+                                    </List.Item>
+                                    <List.Item>
+                                        <List.Content>
+                                            <a href="/" style={{color: 'black'}}>О платформе</a>
+                                        </List.Content>
+                                    </List.Item>
+                                    <List.Item>
+                                        <List.Content>
+                                            <a href="/signup" style={{color: 'black'}}>Регистрация</a>
+                                        </List.Content>
+                                    </List.Item>
+                                    <List.Item>
+                                        <Divider />
+                                    </List.Item>
+                                    <List.Item>
+                                        <List.Content>
+                                            <a href="/" style={{color: 'black', fontWeight: 'bold'}}>RU</a>
+                                        </List.Content>
+                                    </List.Item>
+                                    <List.Item>
+                                        <List.Content>
+                                            <a href="/" style={{color: 'black', fontWeight: 'bold'}}>EN</a>
+                                        </List.Content>
+                                    </List.Item>
+                                </List>
+                            </Segment>
+                        </Portal>
                     <div id="login">
                         <svg className="Rectangle_3">
                             <rect id="Rectangle_3" rx="16" ry="16" x="0" y="0" width="75" height="32">
@@ -110,7 +170,8 @@ class MobileAppHeader extends Component {
                          </a>
                         </div>
                     </div>
-                )}
+                    </div>
+                    )}
             </div>
         )
     }
