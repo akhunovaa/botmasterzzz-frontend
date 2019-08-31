f653ffa23261f0b491cfcf811fcff4d12be960ea
Azat Akhunov <azat.akhunov@protonmail.com>
2019-08-31 10:04:23 +0300

    [NF] loading indicator

--- src/common/LoadingIndicator.js
+++ src/common/LoadingIndicator.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/style.css';
+import {Loader, Dimmer, Segment} from "semantic-ui-react";
 
 export default function LoadingIndicator(props) {
     return (
         <div className="loading-indicator" style={{display: 'block', textAlign: 'center', marginTop: '30px'}}>
-            Загрузка ...
+            <Segment>
+                <Dimmer active inverted>
+                    <Loader size='large'>Загрузка...</Loader>
+                </Dimmer>
+            </Segment>
         </div>
     );
 }
