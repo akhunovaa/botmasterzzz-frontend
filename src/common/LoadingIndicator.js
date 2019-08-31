ab41eeabb9bdceea21081e9b12a5f5dc88d0053a
Azat Akhunov <azat.akhunov@protonmail.com>
2019-08-31 10:14:02 +0300

    [NF] bot token update function injected

--- src/common/LoadingIndicator.js
+++ src/common/LoadingIndicator.js
@@ -5,11 +5,9 @@ import {Loader, Dimmer, Segment} from "semantic-ui-react";
 export default function LoadingIndicator(props) {
     return (
         <div className="loading-indicator" style={{display: 'block', textAlign: 'center', marginTop: '30px'}}>
-            <Segment>
                 <Dimmer active inverted>
                     <Loader size='large'>Загрузка...</Loader>
                 </Dimmer>
-            </Segment>
         </div>
     );
 }
